bcf6045e01a580d38e101788c9232f4c7ff19b3a
laborlab <wdjelhiy@masonlive.gmu.edu>
2021-06-05 18:55:52 -0400

    Pushed the quick_table functions

--- helpers/libraries.R
+++ helpers/libraries.R
@@ -5,11 +5,48 @@ library(data.table)
 library(readxl)
 library(stringdist)
 library(stringr)
-library(RWeka)
+#Sys.setenv('JAVA_HOME'="C:/Program Files/Java/jdk-11.0.1/") 
+#library(RWeka)
 library(tm)
+library(DT)
+library(janitor)
 
 
-df_loader<- function(df,x){
-  df <-list_data$x
-  return(df)
-  }
+
+# df_loader<- function(df,x){
+#   df <-list_data$x
+#   return(df)
+# }
+# 
+# 
+# 
+# clean_data <- function(data){
+#   data %>%
+#     filter(grepl('*', H_MEAN))
+#   
+# }
+
+
+clean_data <- function(data){
+  data <-df_MSA_2010 %>%
+    filter(!str_detect(H_MEAN,'#')) 
+  
+}
+
+quick_table <- function(data, year){
+  
+  d <-data %>%
+    clean_names(case="all_caps") %>%
+    filter(!str_detect(OCC_TITLE,'All Occupations'))%>%
+    filter(!str_detect(OCC_TITLE,'Management Occupations')) %>%
+    filter(str_detect(AREA_NAME,'Washington-Arlington')) %>%
+    select(OCC_TITLE,AREA_NAME,TOT_EMP) %>%
+    datatable(colnames = c('Title', 'Area', 'Jobs'),caption = year)
+    
+ return(d)   
+}
+
+
+
+
+ 
--- source/Loading_data.R
+++ source/Loading_data.R
@@ -22,8 +22,18 @@ names(list_data)<-filenames
 
 df_MSA_2010<-list_data$MSA_M2010_dl_1.csv
 
+df_MSA_2011 <-list_data$MSA_M2011_dl_1_AK_IN.csv
+
+df_MSA_2012 <-list_data$MSA_M2012_dl_1_AK_IN.csv
+
+df_MSA_2013 <-list_data$MSA_M2013_dl_1_AK_IN.csv
+
 df_MSA_2015 <-list_data$aMSA_M2015_dl.csv
 
 df_MSA_2019 <-list_data$MSA_M2019_dl.csv
 
-df_MSA_2011 <-list_data$MSA_M2011_dl_1_AK_IN.csv
+rm(list_data)
+
+#df_MSA_2015_clean <-clean_data(df_MSA_2015)
+
+#quick_table(df_MSA_2015)
