d491cd593c20352e7f4886f48d361f3c01c359ac
laborlab <wdjelhiy@masonlive.gmu.edu>
2021-07-04 12:17:22 -0400

    model start

--- source/Data_Prep_For_Model.R
+++ source/Data_Prep_For_Model.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggplot2)
+library(fpp2)
 library(lubridate)
 df_group_by_year_job <- df_final_matches%>%
   # mutate_at(vars(H_MEAN), as.factor) %>%
@@ -8,7 +9,8 @@ df_group_by_year_job <- df_final_matches%>%
   # mutate_at(vars(Year), as.factor) %>%
   summarise(mean_of_mean= mean(H_MEAN, na.rm = TRUE)) %>%
   mutate(Date=make_date(Year,month = 1L, day=1L)) %>%
-  select(-Year)
+  select(-Year) %>%
+  arrange(Date)
 
 df_cloud_eng <- df_group_by_year_job %>%
   filter(Allwyn =="Cloud Engineer") %>%
@@ -24,8 +26,24 @@ df_cloud_eng <- df_group_by_year_job %>%
   select(mean_of_mean) %>%
   ts(start = 2010)
 
-autoplot(df_cloud_eng$mean_of_mean)
+t<- ts(df_cloud_eng$mean_of_mean)
+
+t<- ts(df_cloud_eng)
+
+autoplot(t)
 
 class(df_cloud_eng)
 
-qplot(mean_of_mean, data = as.data.frame(df_cloud_eng))
+qplot(df_cloud_eng$Date,df_cloud_eng$mean_of_mean)
+
+autoplot(df_cloud_eng$mean_of_mean)
+
+fit <- meanf(t, h=5)
+
+plot(fit)
+
+?meanf
+
+?tslm
+
+summary(fit)
